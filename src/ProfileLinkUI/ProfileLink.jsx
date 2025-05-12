b471961e5e7634f07c10bfe279fc689730ac0e86
AppaiahKelowna <appaiahtwokelowna@gmail.com>
2025-05-11 20:06:22 -0700

    updated 20% of implememttation of Add New Link

--- src/ProfileLinkUI/ProfileLink.jsx
+++ src/ProfileLinkUI/ProfileLink.jsx
@@ -2,13 +2,11 @@ import "./ProfileLink.css";
 
 const ProfileLink = () => {
     return (
-        <>
-            <div className="my-link">
+             <div className="my-link">
                 <i class="fab fa-youtube"></i>
                 <a>My profile Website</a>
                 <span id="remove-link" class="remove-link">&times;</span>
             </div>
-        </>
     )
 };
 
--- src/RightSideMyLink/RightSideMyLink.jsx
+++ src/RightSideMyLink/RightSideMyLink.jsx
@@ -1,13 +1,21 @@
 import ProfileLink from "../ProfileLinkUI/ProfileLink";
 import "./RightSideLink.css";
+import { useState } from "react";
 
 const RightSideMyLink = () => {
+    const [_list, setList] = useState([]);
+
+    const addNewLink = () => {
+        // add <ProfileLink/> to the list below
+        setList([_list,"new item"]);
+    }
+    console.log(_list);
     return (
         <>
             <div className="right-side-wrapper">
                 <h1>My Links</h1>
-                <ProfileLink></ProfileLink>
-                <button>Add new Link</button>
+                {/* 1. append ProfileLink list here  */}
+                <button onClick={addNewLink}>Add new Link</button>
             </div>
         </>
     )
