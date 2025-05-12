8a77c4806d4bfb1086a65500f61c6fb082d9a397
AppaiahKelowna <appaiahtwokelowna@gmail.com>
2025-05-12 11:50:21 -0700

    button type changed from div to form and validation added

--- src/ContactMe/ContactMe.jsx
+++ src/ContactMe/ContactMe.jsx
@@ -2,25 +2,25 @@ import "./ContactMe.css";
 
 const ContactMe = () => {
     return (
-        <div className="ContactMeWrapper">
+        <form className="ContactMeWrapper">
             <h1>Contact Me</h1>
             <div className="name">
                 <label>Name</label>
-                <input type="text"></input>
+                <input type="text" required></input>
             </div>
             <div className="email">
                 <label>Email</label>
-                <input type="text"></input>
+                <input type="email" required></input>
             </div>
             <div className="message">
                 <label>Message</label>
-                <input className="input-message" type="text"></input>
+                <input className="input-message" type="text" required></input>
             </div>
             <div className="contact-me-buttons">
                 <button>Send Message</button>
                 <button>Other Button</button>
             </div>
-        </div>
+        </form>
     )
 }
 
